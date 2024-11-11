96c5a6b5a794e5b5002823a688988b5cf8d2b2d1
Vitor Albuquerque <vitor.s.albq@gmail.com>
2024-11-11 12:14:46 -0300

    feat: prova py 02

---
+++ python/prova02/app.py
@@ -0,0 +1,11 @@
+nome = input("Digite o nome do contato: ")
+telefone = input("Digite o número de telefone: ")
+email = input("Digite o endereço de email: ")
+
+contato = {
+    "nome": nome,
+    "telefone": telefone,
+    "email": email
+}
+
+print(contato)
